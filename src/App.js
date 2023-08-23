f174af344e4b14504a7572b9647412435f4ddcbd
SAUZET-Leny <leny.sauzet@gmail.com>
2023-08-23 17:46:56 +0200

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './home';
 
 function App() {
-  const title = 'welcome to the new blog';
-  const likes = 50;
-  const link = "https://www.google.com";
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Likes { likes } times</p>
-
-        <p>{10}</p>
-        <p>{'hello !'}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{Math.random() * 10}</p>
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
     </div>
   );
