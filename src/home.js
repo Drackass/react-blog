65ecfcb8cdab7798cfc1aeb3db26064efcdad9f4
SAUZET-Leny <leny.sauzet@gmail.com>
2023-08-23 21:31:56 +0200

    Click Events

--- src/home.js
+++ src/home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('hello world',e);
+    }
+
+    const handleClickAgain = (name,e) => {
+        console.log('hello ' + name,e.target);
+    }
+    
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>click me</button>
+            <button onClick={(e) => handleClickAgain('mario',e)}>click me again</button>
         </div>
      );
 }
