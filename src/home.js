ed1a1856d5e9d8931c182c507cbc8d2764ba6f30
SAUZET-Leny <leny.sauzet@gmail.com>
2023-08-24 22:49:42 +0200

    Reusing Components

--- src/home.js
+++ src/home.js
@@ -10,6 +10,7 @@ const Home = () => {
     return ( 
         <div className="Home">
             <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's blogs"/>
         </div>
      );
 }
