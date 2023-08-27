4237688e7495244accb57625bf901c8c926da527
SAUZET-Leny <leny.sauzet@gmail.com>
2023-08-27 17:47:49 +0200

    Reusing Custom Hooks & Controlled Inputs (forms)

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,23 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
-    const { id } = useParams()
-    
-    return ( 
-        <div className="blog-details">
-            <h2>Blog details - { id }</h2>
-        </div>
-     );
+  const { id } = useParams();
+  const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+
+  return (
+    <div className="blog-details">
+      { isPending && <div>Loading...</div> }
+      { error && <div>{ error }</div> }
+      { blog && (
+        <article>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+          <div>{ blog.body }</div>
+        </article>
+      )}
+    </div>
+  );
 }
  
 export default BlogDetails;
